b0657c619afe4412983478bc0870734ebb0d12d8
Steve Foley <sfoley1988@gmail.com>
2017-08-16 15:10:19 -0400

    Clear entire line before printing status

--- watch-dl.py
+++ watch-dl.py
@@ -113,9 +113,8 @@ def downloader(fileurl, file_name):
         file_size_dl += len(buffer)
         f.write(buffer)
         status = r"[download]  %s of %s [%3.2f%%]" % (convertSize(file_size_dl), convertSize(file_size), file_size_dl * 100. / file_size)
-        status = status + chr(8)*(len(status)+1)
-        #print status
-        sys.stdout.write(" %s" % status)
+        sys.stdout.write((" " * (int(os.environ.get("COLUMNS") or 80)-2)) + "\r")
+        sys.stdout.write(status)
         sys.stdout.flush()
 
     #Download done. Close file stream
